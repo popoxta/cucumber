82a9a9be0ba8b248dbf0b81d055d68f4a7a7a686
popoxta <popoxta@gmail.com>
2023-10-09 14:35:10 +1300

    Setup boilerplate step definitions

---
+++ shouty/features/step_definitions/steps.js
@@ -0,0 +1,13 @@
+const { Given, When, Then } = require('@cucumber/cucumber')
+
+Given('Lucy is located {int} metres from Sean', (int) => {
+    return 'pending'
+})
+
+When('Sean shouts {string}', (string) => {
+    return 'pending'
+})
+
+Then('Lucy hears Sean\'s message', () => {
+    return 'pending'
+})
